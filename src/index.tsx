a55f8b970d80c1711237b571f2d2da47e1112deb
Ilyass-shw <souwain.malak@gmail.com>
2021-11-17 12:27:58 +0100

    add react bootstrap

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,9 @@ import { render } from "react-dom";
 import { Provider } from "react-redux";
 import App from "./components/app/App";
 import { store } from "./components/app/store";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+
 render(
   <Provider store={store}>
     <App />
